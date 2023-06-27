f6de4d5f3c0bdf051fa6de94438ad0f1523ad2be
konan <miyagikonan.2023.kensyu@gmail.com>
2023-06-27 16:52:35 +0900

    デザイン３

--- src/main/java/com/example/Kuchannel/form/CreateForm.java
+++ src/main/java/com/example/Kuchannel/form/CreateForm.java
@@ -8,15 +8,15 @@ import org.hibernate.validator.constraints.Length;
 public class CreateForm {
     private int id;
 
-    @NotEmpty(message = "ユーザーIDは必須です")
+    @NotEmpty(message = "※ユーザーIDは必須です")
     @Length (min = 1, max = 50)
     private String loginId;
 
-    @NotEmpty(message = "名前は必須です")
+    @NotEmpty(message = "※名前は必須です")
     @Length (min = 1, max = 50)
     private String name;
 
-    @NotEmpty(message = "パスワードは必須です")
+    @NotEmpty(message = "※パスワードは必須です")
     @Length (min = 1, max = 50)
     private String password;
 
--- src/main/java/com/example/Kuchannel/form/UserForm.java
+++ src/main/java/com/example/Kuchannel/form/UserForm.java
@@ -6,9 +6,9 @@ import lombok.Data;
 @Data
 public class UserForm {
 
-    @NotEmpty(message = "ユーザーIDは必須です")
+    @NotEmpty(message = "※ユーザーIDは必須です")
     private String loginId;
 
-    @NotEmpty(message = "パスワードは必須です")
+    @NotEmpty(message = "※パスワードは必須です")
     private String password;
 }
